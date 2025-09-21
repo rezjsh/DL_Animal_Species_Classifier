7f6962d0643f7c046158e5cc3b344bcba406106d
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-09-21 15:04:02 +0330

    cleanup and adding setup_gpu_memory_growth

--- Animal_Classification/main.py
+++ Animal_Classification/main.py
@@ -1,21 +1,23 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.pipeline.stage_02_data_validation import DataValidationPipeline
+from src.pipeline.stage_03_prepare_dataset import PrepareDatasetPipeline
 from src.pipeline.stage_04_model import AnimalSpeciesClassifierModelPipeline
 from src.pipeline.stage_05_model_trainer import ModelTrainerPipeline
 from src.pipeline.stage_06_model_evaluation import ModelEvaluationPipeline
-from src.config.configuration import ConfigurationManager
-from src.pipeline.stage_02_data_validation import DataValidationPipeline
-from src.pipeline.stage_03_prepare_dataset import PrepareDatasetPipeline
+from src.pipeline.stage_07_model_prediction import ModelPredictionPipeline
 from src.utils.logging_setup import logger
+from src.config.configuration import ConfigurationManager
 if __name__ == '__main__':
     try:
         config_manager = ConfigurationManager()
+        setup_gpu_memory_growth()
 
         # --- Data Ingestion Stage ---
         STAGE_NAME = "Data Ingestion Stage"
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         data_ingestion_pipeline = DataIngestionPipeline(config=config_manager)
         data_ingestion_pipeline.run_pipeline()
-    
+
         # --- Data Validation Stage ---
         STAGE_NAME = "Data Validation Stage"
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -30,12 +32,13 @@ if __name__ == '__main__':
         train_ds, val_ds, test_ds, class_names = dataset_preparation_pipeline.run_pipeline()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-        # --- Create Model Stage ---
+        # --- Creating Model ---
         STAGE_NAME = "Create Model"
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         model_pipeline = AnimalSpeciesClassifierModelPipeline(config=config_manager)
         model = model_pipeline.run_pipeline(class_names=class_names)
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        print('model is', model)
 
         # --- Model Training Stage ---
         STAGE_NAME = "Model Training Stage"
