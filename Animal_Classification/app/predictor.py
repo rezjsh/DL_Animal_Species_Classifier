a2034f8d3161eb25fb65410a57b64e7031d2db17
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-09-22 16:49:02 +0330

    cleanup

--- Animal_Classification/app/predictor.py
+++ Animal_Classification/app/predictor.py
@@ -1,6 +1,9 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input as efficientnet_preprocess
+from tensorflow.keras.applications.resnet_v2 import preprocess_input as resnet_preprocess
+from src.utils.logging_setup import logger
 
 class AnimalPredictor:
     def __init__(self, 
@@ -23,7 +26,6 @@ class AnimalPredictor:
                 'cane', 'cavallo', 'elefante', 'farfalla', 'gallina',
                 'gatto', 'mucca', 'pecora', 'ragno', 'scoiattolo'
             ]
-            # Italian to English translation map
             self.translation_map = {
                 'cane': 'dog',
                 'cavallo': 'horse',
